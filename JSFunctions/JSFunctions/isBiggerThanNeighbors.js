4017536b5183516600bafec72e879ff55e15d489
Viktor Slavchev <mr.slavchev@gmail.com>
2013-04-23 21:06:28 +0300

    .

---
+++ JSFunctions/JSFunctions/isBiggerThanNeighbors.js
@@ -0,0 +1,16 @@
+﻿function compareToNeighbours(myArray, element) {  
+    var isInRange = ((element > 0) && (element < myArray.length - 1)); // here is the funct for exercise 7
+    var output;                                                        // we put in outer file to avoid code copy-paste -ing
+    if (isInRange) {                                                   
+        if ((myArray[element] > myArray[element - 1]) && (myArray[element] > myArray[element + 1])) {
+            output = true;
+        }
+        else {
+            output = false;
+        }
+    }
+    else {
+        output = false; // we modify this part so it just returns false, for compatiability
+    }
+    return output;
+}
